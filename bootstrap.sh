f063843e719bca6ae8af9b39f1a1983a8236a397
anoop <anoop.hallur@gmail.com>
2014-05-17 06:25:57 -0400

    setting up vim automatically

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
 if [ -f ~/.zshrc ]; then 
-    printf "Backing up existing zshrc... "
+    printf "Backing up existing .zshrc ... "
     mv ~/.zshrc ~/.zshrc.bkp
     printf "Done\n"
 fi
 
-printf "Setting up zshrc... "
+printf "Setting up .zshrc ... "
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+printf "Done\n"
+
+
+if [ -f ~/.vimrc ]; then 
+    printf "Backing up existing .vimrc ... "
+    mv ~/.vimrc ~/.vimrc.bkp
+    printf "Done\n"
+fi
+
+
+if [ -e ~/.vim ]; then 
+    printf "Backing up existing .vim ... "
+    mv ~/.vim ~/.vim.bkp
+    printf "Done\n"
+fi
+
+printf "Setting up .vim folder "
+ln -s ~/dotfiles/vim ~/.vim
+printf "Done\n"
+printf "Setting up .vimrc.. "
+ln -s ~/.vim/vimrc ~/.vimrc
 printf "Done\n"
---
+++ vim/README.md
@@ -0,0 +1,10 @@
+VimConfig
+=========
+
+To use this vimconfig
+o
+
+    git clone https://github.com/anooprh/VimConfig.git
+    git submodule foreach git pull
+    mv VimConfig ~/.vim
+    ln -s ~/.vim/vimrc ~/.vimrc
