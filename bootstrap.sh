42bb3455672ad03d96ea2abef90b4fffa6a1dc90
anoop <anoop.hallur@gmail.com>
2014-05-17 23:02:52 -0400

    added tmux conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,6 @@ if [ -f ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc.bkp
     printf "Done\n"
 fi
-
 printf "Setting up .zshrc ... "
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 printf "Done\n"
@@ -16,17 +15,24 @@ if [ -f ~/.vimrc ]; then
     mv ~/.vimrc ~/.vimrc.bkp
     printf "Done\n"
 fi
-
-
 if [ -e ~/.vim ]; then 
     printf "Backing up existing .vim ... "
     mv ~/.vim ~/.vim.bkp
     printf "Done\n"
 fi
-
 printf "Setting up .vim folder "
 ln -s ~/dotfiles/vim ~/.vim
 printf "Done\n"
 printf "Setting up .vimrc.. "
 ln -s ~/.vim/vimrc ~/.vimrc
 printf "Done\n"
+
+if [ -f ~/.tmux.conf ]; then 
+    printf "Backing up existing .tmux.conf ... "
+    mv ~/.tmux.conf ~/.tmux.conf.bkp
+    printf "Done\n"
+fi
+printf "Setting up .tmux.conf ... "
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+printf "Done\n"
+
