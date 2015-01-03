71d7c896b469559c30c75e4f8a20df2a7aba4b41
anooprh <anoop.hallur@gmail.com>
2015-01-04 02:34:15 +0530

    emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,2 +1,14 @@
 (set-foreground-color "yellow")
 (set-background-color "black")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs.d/my-noexternals.el
@@ -0,0 +1,10 @@
+; ~/.emacs.d/my-noexternals.el
+
+;; Setting Brackground color Yellow, foreground black
+(set-foreground-color "yellow")
+(set-background-color "black")
+
+;disable backup
+(setq backup-inhibited t)
+;disable auto save
+(setq auto-save-default nil)
