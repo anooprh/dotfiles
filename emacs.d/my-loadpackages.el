0ee4e0a6a58b21e3069deeeccb54f411eb3320eb
anooprh <anoop.hallur@gmail.com>
2015-01-04 02:59:08 +0530

    solarized theme for emacs

---
+++ emacs.d/my-loadpackages.el
@@ -0,0 +1,5 @@
+; ~/.emacs.d/my-loadpackages.el
+; loading package
+(load "~/.emacs.d/my-packages.el")
+
+;; This file is for making custom modifications to an of the package I install
--- emacs.d/my-noexternals.el
+++ emacs.d/my-noexternals.el
@@ -1,9 +1,5 @@
 ; ~/.emacs.d/my-noexternals.el
 
-;; Setting Brackground color Yellow, foreground black
-(set-foreground-color "yellow")
-(set-background-color "black")
-
 ;disable backup
 (setq backup-inhibited t)
 ;disable auto save
