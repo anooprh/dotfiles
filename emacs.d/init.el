df1c36d2286bec37b58129087cf65e6d16cef83e
Anoop <hallur@adobe.com>
2015-10-27 15:23:31 -0700

    Updading emacs from master

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,37 +1,371 @@
-; ~/.emacs.d/init.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-hook 'after-init-hook '(lambda ()
-  (load "~/.emacs.d/my-noexternals.el")
-))
+;;;; Anoop's .emacs.d/init.el file
+;;;; v 0.1
+;;;; 10/24/2015
 
-(load "~/.emacs.d/my-loadpackages.el")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;disable backup
-(setq backup-inhibited t)
-;disable auto save
+;; Initializing Package Archives
+(require 'cl)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; List of required packages
+(defvar required-packages
+  '(
+    solarized-theme
+    sr-speedbar
+    elscreen
+    expand-region
+    hideshowvis
+    gh-md  ; Markdown viewer from github realtime
+    markdown-mode
+    diff-hl
+    magit
+;;    yasnippet
+;;    solarized-theme
+;;    chess
+;;    auto-complete
+;;    jedi
+;;    pyde
+;;    pyimpsort
+;;    pymacs
+;;    pysmell
+;;    python
+;;    python-cell
+;;    ac-python
+;;    anaconda-mode
+;;    ein
+;;    elpy
+;;    ipython
+;;    dired+
+;;    transpose-frame
+;;    python-mode
+;;    expand-region
+;;    projectile
+;;    move-text
+;;    helm
+;;    neotree
+) "a list of packages to ensure are installed at launch.")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Method to check if all packages are installed
+(defun packages-installed-p ()
+  (loop for p in required-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+;; If not all packages are installed, check one by one and install the missing ones.
+(unless (packages-installed-p)
+  ;; Check for new packages (package versions)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; Install the missing packages
+  (dolist (p required-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Set the Theme- All the below themes are good
+(load-theme 'tango t)
+;(load-theme 'whiteboard t)
+;(load-theme 'leuven t)
+;(load-theme 'misterioso t)
+;(load-theme 'tango-dark t)
+;(load-theme 'wheatgrass t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;; Propmt y or n instead of yes or no
+(defalias 'yes-or-no-p 'y-or-n-p)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Customizing the cursor
+(setq-default cursor-type 'bar)
+; (set-cursor-color "#ffffff") 
+; Visit https://color.adobe.com/ to pick your fav color
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+
+;; Prevent Creation of Backup files
+(setq make-backup-files nil)
+
+;; Disable auto-save
 (setq auto-save-default nil)
 
+;; Prevent Lockfiles
+(setq create-lockfiles nil)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(custom-enabled-themes (quote (solarized-dark)))
- '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(display-battery-mode t)
- '(display-time-mode t)
- '(ede-project-directories (quote ("/home/anoop/Workspace/SPOJ")))
- '(gud-pdb-command-name "/bin/pdb")
- '(inhibit-startup-screen t)
- '(size-indication-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'upcase-region 'disabled nil)
+;; Disable all sounds
+(setq visible-bell 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Tab indentation
+(setq tab-width 2 indent-tabs-mode nil)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Set column numbers
+(setq column-number-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Set line numbers
+(global-linum-mode 1)
+(setq linum-format " %d ")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Easily move arround windows wiht shift  + arrow key
+(windmove-default-keybindings)
+(setq windmove-wrap-around t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Disable starup screen
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Enable auto-refreshing of all buffers
+(global-auto-revert-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Enable edo mode by default
+(require 'ido)
+(ido-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Turn on highlighting matching brackets when cursor is on one of them
+(show-paren-mode t)
+
+;; Auto Close brackets
+(electric-pair-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Function and shortcut (C-d) to duplicate the current line
+(defun duplicate-line (arg)
+  "Duplicate current line, leaving point in lower line."
+  (interactive "*p")
+
+  ;; Save the point for undo
+  (setq buffer-undo-list (cons (point) buffer-undo-list))
+
+  ;; Local variables for start and end of line
+  (let ((bol (save-excursion (beginning-of-line) (point)))
+        eol)
+    (save-excursion
+
+      ;; Don't use forward-line for this, because you would have
+      ;; to check whether you are at the end of the buffer
+      (end-of-line)
+      (setq eol (point))
+
+      ;; Store the line and disable the recording of undo information
+      (let ((line (buffer-substring bol eol))
+            (buffer-undo-list t)
+            (count arg))
+        ;; Insert the line arg times
+        (while (> count 0)
+          (newline)         ;; because there is no newline in 'line'
+          (insert line)
+          (setq count (1- count)))
+        )
+
+      ;; Create the undo information
+      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
+    ) ; End-of-let
+
+  ;; Put the point in the lowest line and return
+  (next-line arg))
+(global-set-key (kbd "C-d") 'duplicate-line)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Set Title bar to show the name of the full path of the file which is open
+(setq frame-title-format
+	(list (format "%s %%S: %%j " (system-name))
+    	'(buffer-file-name "%f" (dired-directory dired-directory "%b"))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Bind Alt Shift Up/Down to move regions of text up and down
+(defun move-text-internal (arg)
+  (cond
+   ((and mark-active transient-mark-mode)
+    (if (> (point) (mark))
+        (exchange-point-and-mark))
+    (let ((column (current-column))
+          (text (delete-and-extract-region (point) (mark))))
+      (forward-line arg)
+      (move-to-column column t)
+      (set-mark (point))
+      (insert text)
+      (exchange-point-and-mark)
+      (setq deactivate-mark nil)))
+   (t
+    (let ((column (current-column)))
+      (beginning-of-line)
+      (when (or (> arg 0) (not (bobp)))
+        (forward-line)
+        (when (or (< arg 0) (not (eobp)))
+          (transpose-lines arg)
+          (when (and (eval-when-compile
+                       '(and (>= emacs-major-version 24)
+                             (>= emacs-minor-version 3)))
+                     (< arg 0))
+            (forward-line -1)))
+        (forward-line -1))
+      (move-to-column column t)))))
+(defun move-text-down (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines down."
+  (interactive "*p")
+  (move-text-internal arg))
+(defun move-text-up (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines up."
+  (interactive "*p")
+  (move-text-internal (- arg)))
+(global-set-key [M-S-up] 'move-text-up)
+(global-set-key [M-S-down] 'move-text-down)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Start Elscreen by default(gives functionality similar to tabs)
+(elscreen-start)
+
+;; For moving with Ctrl + PgUp/PgDown through screens
+(global-set-key [C-prior] 'elscreen-previous)
+(global-set-key [C-next] 'elscreen-next)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Expand Regions -- Ctrl W feature of intelliJ
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)    ;; Expand   with ctrl =
+(global-set-key (kbd "C-+") 'er/contract-region)  ;; Contract with ctrl shift =
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Remap C-w to cut whole line if no selection is made
+(defun xah-cut-line-or-region ()
+  "Cut current line, or text selection.
+When `universal-argument' is called first, cut whole buffer (respects `narrow-to-region').
+URL `http://ergoemacs.org/emacs/emacs_copy_cut_current_line.html'
+Version 2015-06-10"
+  (interactive)
+  (if current-prefix-arg
+      (progn ; not using kill-region because we don't want to include previous kill
+        (kill-new (buffer-string))
+        (delete-region (point-min) (point-max)))
+    (progn (if (use-region-p)
+               (kill-region (region-beginning) (region-end) t)
+             (kill-region (line-beginning-position) (line-beginning-position 2))))))
+(global-set-key (kbd "C-w") 'xah-cut-line-or-region) 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Remap M-w to copy whole line if no selection is made
+(defun xah-copy-line-or-region ()
+  "Copy current line, or text selection.
+When called repeatedly, append copy subsequent lines.
+When `universal-argument' is called first, copy whole buffer (respects `narrow-to-region').
+URL `http://ergoemacs.org/emacs/emacs_copy_cut_current_line.html'
+Version 2015-09-18"
+  (interactive)
+  (let (ξp1 ξp2)
+    (if current-prefix-arg
+        (progn (setq ξp1 (point-min))
+               (setq ξp2 (point-max)))
+      (progn
+        (if (use-region-p)
+            (progn (setq ξp1 (region-beginning))
+                   (setq ξp2 (region-end)))
+          (progn (setq ξp1 (line-beginning-position))
+                 (setq ξp2 (line-end-position))))))
+    (if (eq last-command this-command)
+        (progn
+          (kill-append "\n" nil)
+          (forward-line 1)
+          (end-of-line)
+          (kill-append (buffer-substring-no-properties (line-beginning-position) (line-end-position)) nil)
+          (message "Line copy appended"))
+      (progn
+        (kill-ring-save ξp1 ξp2)
+        (if current-prefix-arg
+            (message "Buffer text copied")
+          (message "Text copied"))))))
+(global-set-key (kbd "M-w") 'xah-copy-line-or-region)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Enable Folding/Unfolding of code with mouse wherever possible
+;; Customizing HideShowvis package : https://melpa.org/#/hideshowvis
+(autoload 'hideshowvis-enable "hideshowvis" "Highlight foldable regions")
+(autoload 'hideshowvis-minor-mode
+  "hideshowvis"
+  "Will indicate regions foldable with hideshow in the fringe."
+  'interactive)
+
+;; Enabling list of modes for which fold should be active
+(dolist (hook (list
+	       ;; List of modes wher folding should be done
+	       'emacs-lisp-mode-hook
+               'c++-mode-hook
+	       'java-mode-hook
+	      )
+	)
+ (add-hook hook 'hideshowvis-enable))
+(hideshowvis-symbols)
+
+;; Customizing the symbol
+(define-fringe-bitmap 'hideshowvis-hideable-marker [255 255 255 255 255 255 255 255])
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Swap Two windows
+(defun swap-windows ()
+  "If you have 2 windows, it swaps them." 
+  (interactive)
+  (cond ((not (= (count-windows) 2)) (message "You need exactly 2 windows to do this."))
+        (t
+         (let* ((w1 (first (window-list)))
+                (w2 (second (window-list)))
+                (b1 (window-buffer w1))
+                (b2 (window-buffer w2))
+                (s1 (window-start w1))
+                (s2 (window-start w2)))
+           (set-window-buffer w1 b2)
+           (set-window-buffer w2 b1)
+           (set-window-start w1 s2)
+           (set-window-start w2 s1)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Toggle Comment on Line/Selected Region
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)
+        (next-line)))
+(global-set-key (kbd "M-;") 'comment-or-uncomment-region-or-line)
+;; Overriding default behaviour to insert comment at end of line.
+;; Rather toggle comment on whole line/ selected region
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Join Lines with M-j
+(global-set-key (kbd "M-j") (lambda () (interactive) (join-line -1)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Markdown real time
+(add-hook 'markdown-mode-hook 
+          (lambda () 
+             (add-hook 'after-save-hook 'gh-md-render-buffer nil 'make-it-local)))
+; (eval-after-load "markdown-mode"
+;     '(define-key markdown-mode-map (kbd "C-c c") (message "Hello")))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Show git diff in side gutter of emacs
+(global-diff-hl-mode)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
