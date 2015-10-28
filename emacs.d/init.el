7bb99002044ac0a97550628e40a85ceb8f17fc04
hallur <hallur@localhost.localdomain>
2015-10-28 16:31:47 -0700

    Including cmake mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -32,6 +32,7 @@
     ;;    magit
     smooth-scrolling
     cmake-mode
+    helm
     ;;    yasnippet
     ;;    solarized-theme
     ;;    chess
@@ -142,9 +143,9 @@
 (global-auto-revert-mode t)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Enable edo mode by default
-(require 'ido)
-(ido-mode t)
+;; Enable edo mode by default --> Disabling it, trying to use helm
+;; (require 'ido)
+;; (ido-mode t)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Turn on highlighting matching brackets when cursor is on one of them
@@ -391,4 +392,28 @@ Version 2015-09-18"
 
 ;; Upon Paste, if something is selected, replace it rather than paste next to it
 (delete-selection-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Helm config
+(require 'helm)
+(require 'helm-config)
+
+;; (global-set-key (kbd "C-c h") 'helm-command-prefix)
+;; (global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+;; (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+(helm-autoresize-mode t)
+
+(helm-mode t)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
