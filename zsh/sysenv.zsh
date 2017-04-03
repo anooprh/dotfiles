e918aa747e73ea6c47dc1eb1a3be84dc160b8268
Anoop Hallur <anoop@Anoops-MacBook-Pro.local>
2017-04-03 16:18:48 -0700

    Made dotfiles system and sysenv agnostic

--- zsh/sysenv.zsh
+++ zsh/sysenv.zsh
@@ -1,20 +1,20 @@
 # System Specific Properties
 
-export CATALINA_HOME='/usr/local/apache-tomcat-7.0.33'
+#export CATALINA_HOME='/usr/local/apache-tomcat-7.0.33'
 
-export GRADLE_HOME='/Users/anooprh/Softwares/gradle-1.6'
+#export GRADLE_HOME='/Users/anooprh/Softwares/gradle-1.6'
 
-export JASPER_HOME='/Users/anooprh/Softwares/jasperreports-server-cp-5.0.0-bin'
+#export JASPER_HOME='/Users/anooprh/Softwares/jasperreports-server-cp-5.0.0-bin'
 
-export JAVA_HOME='/home/anoop/Softwares/jdk1.8.0_121'
+#export JAVA_HOME='/home/anoop/Softwares/jdk1.8.0_121'
 #export JAVA_HOME='/home/hallur/Softwares/jdk1.7.0_79'
 
-export SCALA_HOME='/home/anoop/Softwares/scala-2.11.18'
+#export SCALA_HOME='/home/anoop/Softwares/scala-2.11.18'
 
 #export JAVA_OPTS='-Xms128m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m -Djava.awt.headless=true'
 
 #export MAVEN_OPTS='-Xms128m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m'
 
-export EC2_HOME=/Users/anooprh/.ec2
+#export EC2_HOME=/Users/anooprh/.ec2
 
-export IP_RESOLVER_URL=http://ipecho.net/plain
+#export IP_RESOLVER_URL=http://ipecho.net/plain
